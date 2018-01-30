59cf210322aefa25b05c27fb40f0eb6ee390bb7a
Palmc <33636533+Palmc@users.noreply.github.com>
2018-01-30 22:34:13 +0100

    Update LibreNMSNginx.bash

--- LibreNMSNginx.bash
+++ LibreNMSNginx.bash
@@ -66,8 +66,8 @@ lower_case_table_names=0 \
 
 systemctl restart mysql
 
-sed -i 's|;date.timezone =|date.timezone = Europe/Madrid|g' /etc/php/7.0/fpm/php.ini
-sed -i 's|;date.timezone =|date.timezone = Europe/Madrid|g' /etc/php/7.0/cli/php.ini
+sed -i 's|;date.timezone =|date.timezone = Etc/UTC|g' /etc/php/7.0/fpm/php.ini
+sed -i 's|;date.timezone =|date.timezone = Etc/UTC|g' /etc/php/7.0/cli/php.ini
 
 phpenmod mcrypt
 systemctl restart php7.0-fpm
